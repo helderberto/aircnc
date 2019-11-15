445b19199a0294065e63f0539c8608ffe2591873
Helder Burato Berto <helder.burato@gmail.com>
2019-11-15 17:58:25 -0300

    chore: routes comment about params

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const routes = express.Router();
 
+// GET, POST, PUT, DELETE
+
+// req.query = Access query params (filters)
+// req.params = Access route params (edit, delete)
+// req.body = Access body from requests (create, edit)
+
 routes.post("/users", (req, res) => {
   return res.json(req.body);
 });
