814e61dd734b494fa7fe9a8ce6e18e3238d11a22
Helder Burato Berto <helder.burato@gmail.com>
2019-11-15 18:55:09 -0300

    feat: spotController create new spots and upload thumbnails

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+
 const SessionController = require("./controllers/SessionController");
+const SpotController = require("./controllers/SpotController");
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 // GET, POST, PUT, DELETE
 
@@ -10,5 +15,6 @@ const routes = express.Router();
 // req.body = Access body from requests (create, edit)
 
 routes.post("/sessions", SessionController.store);
+routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
 module.exports = routes;
