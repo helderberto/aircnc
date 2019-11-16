fe2d0cb82c94f8b5762e641cfdbd9c1715b6dd66
Helder Burato Berto <helder.burato@gmail.com>
2019-11-16 12:41:22 -0300

    feat: frontend routes

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import api from "../../services/api";
+
+function Login() {
+  const [email, setEmail] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post("/sessions", { email });
+
+    const { _id } = response.data;
+
+    if (_id) {
+      localStorage.setItem("user", _id);
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  return (
+    <>
+      <p>
+        Ofereça <strong>spots</strong> para programadores e encontre{" "}
+        <strong>talentos</strong> para sua empresa.
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-mail *</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Seu melhor e-mail"
+          value={email}
+          onChange={handleEmailChange}
+        />
+
+        <button className="btn" type="submit">
+          Entrar
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
